24cdcba1f549dd6eb7ff992e6b6ca1d8b5e74302
selisusanti <seli.susanti@indosystem.com>
2020-11-03 09:19:40 +0700

    get api city and provinces

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/provinces', 'ProvincesController@indexProvinces');
+Route::get('/province', 'ProvincesController@searchProvinces');
+Route::get('/cities', 'ProvincesController@indexCities');
+
